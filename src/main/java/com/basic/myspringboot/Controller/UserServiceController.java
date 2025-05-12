604e27b5a74f4d1bee2d3f0679ee5cc1c6829d3a
hhye0 <jhy030123@naver.com>
2025-05-12 23:39:27 +0900

    25일차

---
+++ src/main/java/com/basic/myspringboot/Controller/UserServiceController.java
@@ -0,0 +1,70 @@
+package com.basic.myspringboot.Controller;
+
+import com.basic.myspringboot.Controller.dto.UserDTO;
+import com.basic.myspringboot.entity.User;
+import com.basic.myspringboot.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserServiceController {
+    private final UserService userService;
+
+    //    @PostMapping
+//    public UserDTO.UserResponse create(@Valid @RequestBody
+//                                       UserDTO.UserCreateRequest request) {
+//        User user = new User();
+//        user.setName(request.getName());
+//        user.setEmail(request.getEmail());
+//
+//        User savedUser = userService.createUser(user);
+//        return new UserDTO.UserResponse(savedUser);
+//    }
+    @PostMapping
+    public ResponseEntity<UserDTO.UserResponse> create(@Valid @RequestBody
+                                                       UserDTO.UserCreateRequest request) {
+        UserDTO.UserResponse createdUser = userService.createUser(request);
+        return ResponseEntity.ok(createdUser);
+    }
+
+    @GetMapping
+    public List<UserDTO.UserResponse> getUsers() {
+        return userService.getAllUsers()
+                .stream()
+                //User -> UserDTO.UserResponse
+                .map(user -> new UserDTO.UserResponse(user))
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO.UserResponse getUserById(@PathVariable Long id) {
+        User existUser = userService.getUserById(id);
+        return new UserDTO.UserResponse(existUser);
+    }
+
+    @GetMapping("/email/{email}/")
+    public UserDTO.UserResponse getUserByEmail(@PathVariable String email) {
+        return new UserDTO.UserResponse(userService.getUserByEmail(email));
+    }
+
+    @PatchMapping("/{email}")
+    public ResponseEntity<UserDTO.UserResponse> updateUser(@PathVariable String email,
+                                                           @Valid @RequestBody UserDTO.UserUpdateRequest useDetail) {
+        UserDTO.UserResponse updatedUser = userService.updateUserByEmail(email, useDetail);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
